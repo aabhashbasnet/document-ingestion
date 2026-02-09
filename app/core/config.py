cff5dd6bfc62be569d8630aa27812403f7e8fc89
ABASNET <abasnet@ssnbuilders.com>
2026-02-09 12:46:46 +0545

    add basic folder structuree and config setup

---
+++ app/core/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Document Ingestion API"
+    API_V1_STR: str = "/api/v1"
+
+    EMBEDDING_MODEL: str = "text-embedding-3-small"
+    EMBEDDING_DIM: int = 1536
+
+    VECTOR_DB_TYPE: str = "chroma"
+    CHROMA_PATH: str = "./chroma_db"
+
+    OPENAI_API_KEY: str | None = None
+
+    mdoel_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_ignore_empty=True,
+        extra="ignore",
+    )
+
+
+settings = Settings()  # global settings object
